d7151a31d9b37587e4615aa562fa959faa5f7cb5
mikebarone <mikebarone@gmail.com>
2017-03-22 18:46:59 -0400

    Email authentication with Firebase

--- Sociapp/SignInVC.swift
+++ Sociapp/SignInVC.swift
@@ -14,6 +14,9 @@ import Firebase
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var passwordField: FancyField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -54,5 +57,33 @@ class SignInVC: UIViewController {
         })
     }
 
+    @IBAction func signinPressed(_ sender: Any) {
+        if let email = emailField.text, let pwd = passwordField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("MIKE: Email user authenticated with Firebase")
+                    if let user = user {
+                        let userData = ["provider": user.providerID]
+                        //self.completeSignIn(id: user.uid, userData: userData)
+                    }
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("MIKE: Unable to authenticate with Firebase using email")
+                        } else {
+                            print("MIKE: Successfully authenticated with Firebase")
+                            FIRAuth.auth()?.currentUser?.sendEmailVerification(completion: { (error) in
+                                print("MIKE: Verification email sent!")
+                            })
+                            if let user = user {
+                                let userData = ["provider": user.providerID]
+                                //self.completeSignIn(id: user.uid, userData: userData)
+                            }
+                        }
+                    })
+                }
+            })
+        }
+    }
 }
 
