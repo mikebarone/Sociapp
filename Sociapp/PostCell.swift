84a70df5ccb5a0a9a8c62311059eddc9375920df
mikebarone <mikebarone@gmail.com>
2017-03-24 21:39:43 -0400

    Downloading images from Firebase

--- Sociapp/PostCell.swift
+++ Sociapp/PostCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PostCell: UITableViewCell {
     
@@ -24,10 +25,29 @@ class PostCell: UITableViewCell {
         // Initialization code
     }
     
-    func configureCell(post: Post){
+    func configureCell(post: Post, img: UIImage? = nil){
         self.post = post
         self.postCaption.text = post.caption
         self.likeNumber.text = "\(post.likes)"
+        
+        if img != nil {
+            self.postImage.image = img
+        } else {
+            let ref = FIRStorage.storage().reference(forURL: post.imageUrl)
+            ref.data(withMaxSize: 2 * 1024 * 1024, completion: { (data, error) in
+                if error != nil {
+                    print("MIKE: Unable to download image from Firebase storage")
+                } else {
+                    print("MIKE: Image downloaded from Firebase storage")
+                    if let imgData = data {
+                        if let img = UIImage(data: imgData) {
+                            self.postImage.image = img
+                            FeedVC.imageCache.setObject(img, forKey: post.imageUrl as NSString)
+                        }
+                    }
+                }
+            })
+        }
         
     }
 
